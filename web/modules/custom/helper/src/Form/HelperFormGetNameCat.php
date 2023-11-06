65ed9ecc5df358e60919a0469053f91c7c55cf51
vhelperv <vladyslav.p014@gmail.com>
2023-11-06 14:49:36 +0200

    Add validation to field name with Ajax API

--- web/modules/custom/helper/src/Form/HelperFormGetNameCat.php
+++ web/modules/custom/helper/src/Form/HelperFormGetNameCat.php
@@ -4,6 +4,8 @@ namespace Drupal\helper\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\MessageCommand;
 
 class HelperFormGetNameCat extends FormBase
 {
@@ -30,6 +32,9 @@ class HelperFormGetNameCat extends FormBase
     $form['submit'] = array(
       '#type' => 'submit',
       '#value' => $this->t('Add cat'),
+      '#ajax' => array(
+        'callback' => '::validateForm',
+      )
     );
     return $form;
   }
@@ -37,8 +42,16 @@ class HelperFormGetNameCat extends FormBase
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state)
-  {
+  public function validateForm(array &$form, FormStateInterface $form_state): AjaxResponse {
+    $name = $form_state->getValue('cat-name');
+    $response = new AjaxResponse();
+    if (mb_strlen($name, 'UTF-8') < 2 || mb_strlen($name, 'UTF-8') > 32) {
+      $response->addCommand(new MessageCommand('The name must be between 2 and 32 characters long.',NULL, ['type' => 'error']));
+    }
+    else {
+      $response->addCommand(new MessageCommand('The name is valid.',NULL));
+    }
+    return $response;
   }
 
   /**
